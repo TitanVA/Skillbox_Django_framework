29d13cdd717843c1f0bf1ca556d85ac2400b3a70
Viktor Bezai <32422942+TitanVA@users.noreply.github.com>
2021-10-29 13:32:44 +0300

    Paginator

--- TestSite/news/admin.py
+++ TestSite/news/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "category", "created_at", "photo", "is_published")
+    list_display = ("id", "title", "category", "created_at", "get_photo", "is_published")
     list_display_links = ("id", "title")
     search_fields = ("title", "content")
     list_editable = ("is_published",)
     list_filter = ("is_published", "category")
+    fields = ("title", "category", "content", "photo", "get_photo", "is_published", "views", "created_at", "updated_at")
+    readonly_fields = ("get_photo", "views", "created_at", "updated_at")
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width='75'>")
+        else:
+            return "No photo"
 
 
 class CategoryAdmin(admin.ModelAdmin):
--- TestSite/news/views.py
+++ TestSite/news/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import News, Category
 from .forms import NewsForm
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+
+
+def test(request):
+    objects = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get("page", 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, "news/test.html", {"page_obj": page_objects})
 
 
 class HomeNews(ListView):
     model = News
     template_name = "news/home_news_list.html"
     context_object_name = "news"
-    # extra_context = {"title": "Главная"}
+    mixin_prop = "Hello World"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
@@ -16,7 +26,7 @@ class HomeNews(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related("category")
 
 # def index(request):
 #     news = News.objects.order_by("-created_at")
@@ -30,7 +40,6 @@ class NewsByCategory(ListView):
     model = News
     template_name = "news/home_news_list.html"
     context_object_name = "news"
-    allow_empty = False
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(NewsByCategory, self).get_context_data(**kwargs)
@@ -38,7 +47,7 @@ class NewsByCategory(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs["category_id"], is_published=True)
+        return News.objects.filter(category_id=self.kwargs["category_id"], is_published=True).select_related("category")
 
 # def get_category(request, category_id):
 #     news = News.objects.filter(category_id=category_id)
@@ -55,21 +64,24 @@ class ViewNews(DetailView):
     # pk_url_kwarg = "news_id"
     # template_name = "news/news_detail.html"
 
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, "news/view_news.html", {"news_item": news_item})
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, "news/view_news.html", {"news_item": news_item})
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = "news/add_news.html"
+    # success_url = reverse_lazy("home")
 
-
-def add_news(request):
-    if request.method == "POST":
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, "news/add_news.html", {"form": form})
+# def add_news(request):
+#     if request.method == "POST":
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, "news/add_news.html", {"form": form})
