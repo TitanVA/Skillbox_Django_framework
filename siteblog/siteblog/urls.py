24f298e23562e6e7518873a8a11d115658f77439
Viktor Bezai <32422942+TitanVA@users.noreply.github.com>
2021-11-05 11:07:19 +0300

    test

--- siteblog/siteblog/urls.py
+++ siteblog/siteblog/urls.py
@@ -1,4 +1,4 @@
-"""TestSite URL Configuration
+"""siteblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,19 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("news.urls")),
-
 ]
-
-if settings.DEBUG:
-    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
